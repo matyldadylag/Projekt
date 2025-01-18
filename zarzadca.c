0709f5a2022778ec5b1cdd7e9d8233d2cf4af70d
Matylda Dylag <matylda.dylag@outlook.com>
2025-01-18 11:58:35 +0100

    Poprawki w zarządcy

--- zarzadca.c
+++ zarzadca.c
@@ -8,14 +8,23 @@ time_t* czas_otwarcia;
 // Obsługa sygnału SIGINT
 void SIGINT_handler(int sig)
 {
-    // Wysyła SIGINT do procesów kasjera i ratownika
+    // Wysyła SIGINT do procesów kasjera i ratowników
     kill(PID_kasjera, SIGINT);
     kill(PID_ratownika_olimpijski, SIGINT);
     kill(PID_ratownika_rekreacyjny, SIGINT);
     kill(PID_ratownika_brodzik, SIGINT);
     
+    // Usuwa pamięć dzieloną
     shmctl(ID_pamieci, IPC_RMID, 0);
     shmdt(czas_otwarcia);
+
+    printf("[%s] Zarządca czeka, aż wszyscy opuszczą kompleks basenów\n", timestamp());    
+
+    while (wait(NULL) > 0);
+
+    printf("[%s] Kompleks basenów jest zamknięty\n", timestamp());
+
+    printf("[%s] Zarządca zakończył pracę\n", timestamp());
 
     exit(0);
 }
@@ -25,20 +34,22 @@ int main()
     // Obsługa sygnału SIGINT
     signal(SIGINT, SIGINT_handler);
 
-    int czas_pracy;
+    // Komunikat o uruchomieniu zarządcy
+    printf("[%s] Zarządca uruchomiony\n");
 
-    printf("Czas otwarcia basenu: %s\n", timestamp());
-    printf("Podaj czas pracy basenu (w sekundach): ");
-    scanf("%d", &czas_pracy);
-
+    // Obsługa czasu działania programu
     // Utworzenie pamięci dzielonej do przechowywania zmiennej czas_otwarcia
     key_t klucz_pamieci = ftok(".", 3213);
     ID_pamieci = shmget(klucz_pamieci, sizeof(time_t), 0600 | IPC_CREAT);
     czas_otwarcia = (time_t*)shmat(ID_pamieci, NULL, 0);
-
-    // Ustawienie czasu otwarcia, do którego inne procesy będą miały dostęp
-    *czas_otwarcia = time(NULL);
-
+    printf("Podaj czas pracy basenu (w sekundach): ");
+    int czas_pracy;
+    scanf("%d", &czas_pracy); // Użytkownik podaje czas pracy programu
+    // Komunikat o otwarciu kompleksu basenów
+    *czas_otwarcia = time(NULL); // Ustawienie czasu otwarcia, do którego inne procesy będą miały dostęp w pamięci dzielonej
+    printf("[%s] Kompleks basenów jest otwarty\n", timestamp());
+    time_t czas_zamkniecia = *czas_otwarcia + czas_pracy; // Ustalenie czasu zamknięcia
+    
     // Uruchomienie kasjera
     PID_kasjera = fork();
     if(PID_kasjera == 0)
@@ -73,9 +84,6 @@ int main()
 
     // Generowanie klientów w losowych odstępach czasu
     pid_t PID_klienta;
-
-    time_t czas_zamkniecia = *czas_otwarcia + czas_pracy;
-
     while (time(NULL) < czas_zamkniecia)
     {
         PID_klienta = fork();
@@ -87,16 +95,8 @@ int main()
         sleep(rand()%10);
     }
 
-    sleep(60);
-
-    kill(PID_kasjera, SIGINT);
-    kill(PID_ratownika_olimpijski, SIGINT);
-    kill(PID_ratownika_rekreacyjny, SIGINT);
-    kill(PID_ratownika_brodzik, SIGINT);
-    shmctl(ID_pamieci, IPC_RMID, 0);
-    shmdt(czas_otwarcia);
-
-    while (wait(NULL) > 0);
+    // Gdy nadejdzie czas zamknięcia, zarządca zamyka kompleks basenów i kończy działanie
+    raise(SIGINT);
 
     return 0; 
 }
